90bc2be944f2782d50f15b4c25f7d16cc172b270
Dan Bergelt <dan@danbergelt.com>
2020-02-15 20:52:39 -0500

    remove webhook

--- src/routes/webhooks.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from 'express';
-import { rebuild } from '../controllers/webhooks';
-
-const router: Router = express.Router();
-
-router.route('/rebuild').post(rebuild);
-
-export default router;
