9d4d8ae6bd3a20dba3991296502c46840904ac42
Dan Bergelt <dan@danbergelt.com>
2020-02-16 00:05:18 -0500

    fixing staging api image name

--- deploy.staging.sh
+++ deploy.staging.sh
@@ -3,4 +3,4 @@
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 echo Building staging API...
 docker-compose -f docker-compose.staging.yml build staging_api
-docker push danbergelt/spotter-be_staging_api:latest
+docker push danbergelt/staging_api:latest
