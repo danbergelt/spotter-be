69be433fa6b0577ca00ed945118d5007d6c2485e
Dan Bergelt <dan@danbergelt.com>
2020-02-15 23:47:19 -0500

    fixed deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 echo Building production API...
-echo docker-compose -f docker-compose.production.yml build prod_api
-echo docker tag spotter-be_prod_api:latest danbergelt/spotter-be_prod_api:latest
-echo docker push danbergelt/spotter-be_prod_api:latest
+docker-compose -f docker-compose.production.yml build prod_api
+docker tag spotter-be_prod_api:latest danbergelt/spotter-be_prod_api:latest
+docker push danbergelt/spotter-be_prod_api:latest
